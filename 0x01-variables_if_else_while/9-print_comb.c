389b95f6bbf25cd2fe2020f907fbc0008f47cc27
hodz <elkhemlichi.mehdi3@gmail.com>
2023-03-17 17:43:08 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+/* This program prints the numbers from 0 to 9 separated by commas and spaces.
+   It uses a for loop to iterate through the numbers and putchar() function to print them.
+*/
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 48; num <= 57; num++)
+	for (i = 48; i <= 57; i++)
 	{
-		putchar(num);
-		if (num != 57)
+		putchar(i);
+		if (i != 57)
 		{
 			putchar(44);
 			putchar(32);
