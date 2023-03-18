1e337c002255a8452baf116d5f8c0c58bc7adb93
hodz <elkhemlichi.mehdi3@gmail.com>
2023-03-17 17:38:23 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
+int main(void)
+{
+	int num;
 
-int main() {
-    int i, j;
-
-    for (i = 0; i <= 8; i++) {
-        for (j = i + 1; j <= 9; j++) {
-            putchar(i + '0');
-            putchar(',');
-            putchar(' ');
-            putchar(j + '0');
-            if (i == 8 && j == 9) {
-                continue;
-            }
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    return 0;
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
 }
